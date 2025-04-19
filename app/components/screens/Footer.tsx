d76fc2261f2087545cb885c43b4f4a9473f83619
DeveloperAromal <153208245+DeveloperAromal@users.noreply.github.com>
2025-04-19 11:15:25 +0530

    commited

--- app/components/screens/Footer.tsx
+++ app/components/screens/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="bg-ocean-800 text-white pt-16 pb-8">
+    <footer className="bg-black text-white pt-16 pb-8">
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {/* About Section */}
